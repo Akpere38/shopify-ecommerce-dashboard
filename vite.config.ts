464a3ead5b005a34f763de509709026071a21ffe
Akpere38 <akpereraphael@gmail.com>
2026-05-01 05:23:34 -0400

    fix: fully remove cloudflare vite plugin and clear cache

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(), tailwindcss(),],
 resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
